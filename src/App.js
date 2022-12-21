4739ad3c62e024db703b80ad3c6c6693aa0831e8
조현홍 <wigwam4@naver.com>
2022-12-21 14:01:47 +0900

    <chore> 기능 개선

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Rㅇㅇㅇ
+          Learn Rㅇㅇㅇadsf
         </a>
       </header>
     </div>
